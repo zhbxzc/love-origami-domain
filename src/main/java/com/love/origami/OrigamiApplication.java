e0c376d2acffdd33718907ba00f21f8b3d99de9d
zhanghb22 <329458839@qq.com>
2016-09-10 15:24:06 +0800

    增加rabbitmq监控

--- src/main/java/com/love/origami/OrigamiApplication.java
+++ src/main/java/com/love/origami/OrigamiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.sleuth.Sampler;
 /*import org.springframework.cloud.sleuth.Sampler;*/
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -26,11 +27,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableCircuitBreaker
 public class OrigamiApplication
 {
-/*	@Bean
+	@Bean
 	Sampler sampler()
 	{
 		return span -> true;
-	}*/
+	}
 
 	public static void main(String[] args)
 	{
